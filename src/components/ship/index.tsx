6149a1912e799600dc1e3ac4c555c94c9042f296
LastSkywalker <maksim.drozd@innowise-group.com>
2023-10-06 15:05:11 +0200

    fix types

--- src/components/ship/index.tsx
+++ src/components/ship/index.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { LoaderProto, MeshProps, useLoader } from '@react-three/fiber';
+import { PrimitiveProps, useLoader } from '@react-three/fiber';
 import { useTexture } from '@react-three/drei';
-import { FC, useEffect, useLayoutEffect, useMemo } from 'react';
+import { FC, useLayoutEffect } from 'react';
 import * as THREE from 'three';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
-export const Ship: FC<MeshProps> = (props) => {
+export const Ship: FC<Omit<PrimitiveProps, 'object'>> = (props) => {
   const [texture, normal, height, roughness, metalness] = useTexture([
     './ship/ship_Base_color.png',
     './ship/ship_Normal_DirectX.png',
@@ -18,17 +18,19 @@ export const Ship: FC<MeshProps> = (props) => {
   const obj = useLoader(OBJLoader as any, './ship/ship.obj');
 
   useLayoutEffect(() => {
-    obj.traverse((child) => {
-      if (child.isMesh) {
-        child.material.map = texture;
-        child.material.normalMap = normal;
-        // child.material.displacementMap = height;
-        child.material.roughnessMap = roughness;
-        child.material.metalnessMap = metalness;
-        child.geometry.computeVertexNormals();
-      }
-    });
+    obj.traverse(
+      (child: THREE.Mesh & { material: THREE.MeshPhysicalMaterial }) => {
+        if (child.isMesh) {
+          child.material.map = texture;
+          child.material.normalMap = normal;
+          // (child.material).displacementMap = height;
+          child.material.roughnessMap = roughness;
+          child.material.metalnessMap = metalness;
+          child.geometry.computeVertexNormals();
+        }
+      },
+    );
   }, [height, metalness, normal, obj, roughness, texture]);
 
-  return <primitive object={obj} />;
+  return <primitive object={obj} {...props} />;
 };

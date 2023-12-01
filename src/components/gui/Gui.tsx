361214bd2580e62b2ae529e2046151788df59719
LastSkywalker <maksim.drozd@innowise-group.com>
2023-12-01 17:26:01 +0100

    fix text color

--- src/components/gui/Gui.tsx
+++ src/components/gui/Gui.tsx
@@ -32,7 +32,7 @@ export const Gui: FC<GuiProps> = ({ map, className, isPlaying, setIsPlaying }) =
   };
 
   return (
-    <div className={cx("w-full h-full", className)}>
+    <div className={cx("w-full h-full text-white", className)}>
       <audio
         ref={audio}
         src="audios/ambient.mp3"
